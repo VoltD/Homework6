6431e1988910b0ef54c49e26090542a50cfe916b
dmitry.volt <d.volt@skyeng.ru>
2024-02-29 15:41:22 +0300

    Tasks 1-5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Task 1
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        // Task 2
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        // Task 3
+        for (int i = 0; i <= 17; i += 2) {
+            if (i == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+        // Task 4
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+
+        // Task 5
+        for (int i = 1904; i <= 2096; i += 4) {
+            System.out.println(i + " год является високосным");
+        }
     }
 }
